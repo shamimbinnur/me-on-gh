d18061867ada13f62dcf2234d67a1d938f5807e8
shamimbinnur <sbnsbn00@gmail.com>
2022-05-13 22:14:40 +0600

    Included SEO data

---
+++ page-content/home-page/homePageSaticData.js
@@ -0,0 +1,10 @@
+const homePageSaticData = {
+    seo:{
+        title: "Me on Gh",
+        metaDesc: "Generate your github profile in one click!",
+        ulr: "localhost",
+        image: ""
+    }
+}
+
+export { homePageSaticData }
