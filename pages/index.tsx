845f007489bb0b0c0bf69a48f41d24756042270c
shamimbinnnur <sbnsbn00@gmail.com>
2022-06-08 00:01:33 +0600

    fixed typos

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,29 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import { homePageSaticData } from '../page-content/home-page/homePageSaticData'
+import { homePageStaticData } from '../page-content/home-page/homePageStaticData'
 
 
 const Home: NextPage = () => {
   return (
     <div>
       <Head>
-        <title>{homePageSaticData.seo.title}</title>
-        <meta name="description" content={homePageSaticData.seo.metaDesc} />
+        <title>{homePageStaticData.seo.title}</title>
+        <meta name="description" content={homePageStaticData.seo.metaDesc} />
         <link rel="icon" href="/favicon.ico" />
 
         {/* <!-- Open Graph / Facebook --> */}
         <meta property="og:type" content="website" />
-        <meta property="og:url" content={homePageSaticData.seo.url} />
-        <meta property="og:title" content={homePageSaticData.seo.title} />
-        <meta property="og:description" content={homePageSaticData.seo.metaDesc} />
-        <meta property="og:image" content={homePageSaticData.seo.image} />
+        <meta property="og:url" content={homePageStaticData.seo.url} />
+        <meta property="og:title" content={homePageStaticData.seo.title} />
+        <meta property="og:description" content={homePageStaticData.seo.metaDesc} />
+        <meta property="og:image" content={homePageStaticData.seo.image} />
 
         {/* <!-- Twitter --> */}
         <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content={homePageSaticData.seo.url} />
-        <meta property="twitter:title" content={homePageSaticData.seo.title} />
-        <meta property="twitter:description" content={homePageSaticData.seo.metaDesc} />
-        <meta property="twitter:image" content={homePageSaticData.seo.image} />
+        <meta property="twitter:url" content={homePageStaticData.seo.url} />
+        <meta property="twitter:title" content={homePageStaticData.seo.title} />
+        <meta property="twitter:description" content={homePageStaticData.seo.metaDesc} />
+        <meta property="twitter:image" content={homePageStaticData.seo.image} />
       </Head>
 
       <h1 className=' text-center text-4xl ' >Me On Gh is comming soon!</h1>
