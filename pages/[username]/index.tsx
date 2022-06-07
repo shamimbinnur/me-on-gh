2c5fd2b57263eeab36b200c1a5d21354a0655d76
shamimbinnnur <sbnsbn00@gmail.com>
2022-06-07 23:58:00 +0600

    Removed getStaticsProps

--- pages/[username]/index.tsx
+++ pages/[username]/index.tsx
@@ -4,50 +4,31 @@ import React from 'react'
 import { homePageSaticData } from '../../page-content/home-page/homePageSaticData'
 
 
-interface AppProps{
-    homePageStaticData:{
-      seo: {
-        title: string,
-        metaDesc: string,
-        url: string,
-        image: string,
-      }
-  
-    }
-  }
 
-const App: NextPage<AppProps> = ({ homePageStaticData: { seo } }) => {
+const App: NextPage= () => {
   return (
     <div>
         <Head>
-        <title>{seo.title}</title>
-        <meta name="description" content={seo.metaDesc} />
+        <title>{homePageSaticData.seo.title}</title>
+        <meta name="description" content={homePageSaticData.seo.metaDesc} />
         <link rel="icon" href="/favicon.ico" />
 
         {/* <!-- Open Graph / Facebook --> */}
         <meta property="og:type" content="website" />
-        <meta property="og:url" content={seo.url} />
-        <meta property="og:title" content={seo.title} />
-        <meta property="og:description" content={seo.metaDesc} />
-        <meta property="og:image" content={seo.image} />
+        <meta property="og:url" content={homePageSaticData.seo.url} />
+        <meta property="og:title" content={homePageSaticData.seo.title} />
+        <meta property="og:description" content={homePageSaticData.seo.metaDesc} />
+        <meta property="og:image" content={homePageSaticData.seo.image} />
 
         {/* <!-- Twitter --> */}
         <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content={seo.url} />
-        <meta property="twitter:title" content={seo.title} />
-        <meta property="twitter:description" content={seo.metaDesc} />
-        <meta property="twitter:image" content={seo.image} />
+        <meta property="twitter:url" content={homePageSaticData.seo.url} />
+        <meta property="twitter:title" content={homePageSaticData.seo.title} />
+        <meta property="twitter:description" content={homePageSaticData.seo.metaDesc} />
+        <meta property="twitter:image" content={homePageSaticData.seo.image} />
       </Head>
     </div>
   )
 }
 
 export default App
-
-
-export async function getServerSideProps() {
-
-  return { props: {
-    homePageStaticData : homePageSaticData
-  } }
-}
