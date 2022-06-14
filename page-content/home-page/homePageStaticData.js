e5b5f4f829219134117b2e36319f50701025a7ab
shamimbinnnur <sbnsbn00@gmail.com>
2022-06-14 21:43:57 +0600

    header nav added'

--- page-content/home-page/homePageStaticData.js
+++ page-content/home-page/homePageStaticData.js
@@ -4,7 +4,21 @@ const homePageStaticData = {
         metaDesc: "Generate your github profile in one click!",
         url: "localhost",
         image: ""
-    }
+    },
+    headerNav:[
+        {
+            about:{
+                link: "#"
+            },
+            doc:{
+                link: "#"
+            },
+            privacyPolicy:{
+                link: "#"
+            },            
+        }
+    ],
+
 }
 
 export { homePageStaticData }
