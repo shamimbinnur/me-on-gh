d08fed472eb03955a0490b450803469b2b2a5d13
shamimbinnnur <sbnsbn00@gmail.com>
2022-07-09 13:17:22 +0600

    Modified static data

--- components/BellowFooter.jsx
+++ components/BellowFooter.jsx
@@ -13,11 +13,11 @@ const BellowFooter = () => {
             <Image className='w-full h-full  scale-[1.4] ' alt='Squres vector' src={squares_vector} />
         </div>
         <div className=' flex gap-[8px] items-center'>
-            <div className='w-[40px] h-auto'>
+            <div className='w-[38px] h-auto'>
                 <Image  alt='Mogh logo' src={MOGH} />
             </div>
             <div className=''>
-                <div className='w-[100px] h-[20px] '>
+                <div className='w-[90px] h-[18px] '>
                     <Image alt='Mogh text' src={MOGH_text} />
                 </div>
                 <p className='text-[10px] text-grayDimMOGH'>Open Source, MIT Licenced</p>
