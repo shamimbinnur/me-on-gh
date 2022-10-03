378f514a6e6d49f2fafabd53d91ca73dbddacd21
shamimbinnnur <sbnsbn00@gmail.com>
2022-10-03 11:03:21 +0600

    reset

--- pages/[username]/index.tsx
+++ pages/[username]/index.tsx
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from 'react'
 import { homePageStaticData } from '../../page-content/home-page/homePageStaticData'
 import { getProfile } from '../../github/gitHubApi'
 import Image from 'next/image'
-// import { profileData } from "../../page-content/dummyContent/profile.js"
+import { profileData } from "../../page-content/dummyContent/profile.js"
 import dpArtwork from "../../public/svg/dpArtwork.svg"
 import { HiOutlineOfficeBuilding, HiLocationMarker } from 'react-icons/hi';
 import { VscFilePdf } from 'react-icons/vsc';
@@ -113,128 +113,13 @@ const App: NextPage<ProfileProps>= ({profileData}) => {
         <meta property="twitter:image" content={avatar_url} />
       </Head>
 
-      <HeaderForProfile/>
-
-      {
-        neetToGreet &&
-        <Greetings greetingsData={{language: MUL, userInfo: { name } }}/>
-      }
-
-      {
-        !neetToGreet &&
-        <div>
-          <div className='flex justify-end mr-9'>
-            <PDFDownloadLink document={<ProfilePdf profileData={ profileData.profileData as any}  />} fileName={`${name}_resume`}>
-            {({ blob, url, loading, error }) =>
-              loading ? (
-                <div className=' bg-red-500 flex items-center px-2 py-2 cursor-pointer rounded-xl'>
-                  ...
-                </div>
-              ) :
-              (
-                <div className=' bg-red-500 flex items-center px-2 py-2 cursor-pointer rounded-xl'>
-                  <VscFilePdf className='text-[1.4rem] text-white '/>
-                </div>
-              )
-            }
-            </PDFDownloadLink>
-          </div>
-
-          <div className="mobile:py-[20px] tablet:min-h-screen mobile:px-[20px] tablet:px-[30px] lapto:px-[10px] mx-auto max-w-6xl">
-            <div className="flex flex-col items-center">
-              <div className="relative flex-1 flex justify-center p-10 items-center">
-                <div className='absolute'>
-                  <Image src={dpArtwork} alt={name} width={400} height={400} />      
-                </div>
-                <div className='rounded-full overflow-hidden p-1 bg-gradient-to-r from-gray-200 to-gray-100 items-center flex'>
-                  <Image className='rounded-full' src={avatar_url} alt={name} width={256} height={256} />      
-    
-                </div>
-              </div>
-              <div className="flex-1">
-                <Link href={html_url as string}>
-                  <h1 className="text-gray-700 cursor-pointer transition ease-in-out duration-500 hover:text-primaryOne font-bold text-[2.5rem]">{name}</h1>
-                </Link>
-                <p className="text-gray-700 font-semibold text-[1rem] my-[10px] max-w-[300px]">{bio}</p>
-                <div>
-                  {
-                    company &&
-                    <div className='flex items-center gap-2 group'>
-                      <div className='bg-primaryTwo p-[4px] group-hover:bg-opacity-20 group-hover:bg-primaryOne rounded-full bg-opacity-20 flex items-center justify-center'>
-                        <HiOutlineOfficeBuilding className='text-primaryTwo group-hover:text-primaryOne transition ease-in-out duration-300 text-[1rem]' />
-                      </div>
-                      <p className="text-gray-600 font-semibold text-[1.1rem]">{company}</p>
-                    </div>
-                  }
-                  {
-                    location &&
-                    <div className='flex items-center group gap-2'>
-                      <div className='bg-primaryTwo p-[4px] group-hover:bg-opacity-20 group-hover:bg-primaryOne transition ease-in-out duration-500 rounded-full bg-opacity-20 flex items-center justify-center'>
-                        <HiLocationMarker className='text-primaryTwo group-hover:text-primaryOne transition ease-in-out duration-300 text-[1rem]' />
-                      </div>
-                      <p className="text-gray-600 font-semibold text-[1.1rem]">{location}</p>
-                    </div>
-                  }
-                  {
-                    blog &&
-                    <div className='flex items-center gap-2 group'>
-                      <div className='bg-primaryTwo p-[4px] group-hover:bg-opacity-20 rounded-full bg-opacity-20 group-hover:bg-primaryOne transition ease-in-out duration-500 flex items-center justify-center'>
-                        <BiWorld className='text-primaryTwo group-hover:text-primaryOne transition ease-in-out duration-300 text-[1rem]' />
-                      </div>
-                      <Link rel="noreferrer" target="_blank" href={"https://"+blog} >
-                        <p className="text-gray-600 font-semibold cursor-pointer text-[1.1rem]">{blog}</p>
-                      </Link>
-                    </div>
-                  }
-    
-                  {
-                    orgs.length > 0 ? (
-                    <div className='flex items-center group gap-2'>
-                      <div className='bg-primaryTwo p-[4px] group-hover:bg-opacity-20 rounded-full bg-opacity-20 group-hover:bg-primaryOne transition ease-in-out duration-500 flex items-center justify-center'>
-                        <GiFamilyHouse className='text-primaryTwo group-hover:text-primaryOne transition ease-in-out duration-300 text-[1rem]' />
-                      </div>
-                      <div className='flex items-center gap-2'>
-                        {
-                          orgs.map(og => (
-                            <div key={og.login}>
-                              <p className="text-gray-600 font-semibold text-[1.1rem]">{og.login}</p>
-                            </div>
-                          ))
-                        }
-                      </div>
-                    </div>
-                    ): ""
-                  }
-                  {
-                    followers &&
-                    <div className='flex items-center group gap-2'>
-                      <div className='bg-primaryTwo p-[4px] group-hover:bg-opacity-20 rounded-full bg-opacity-20 group-hover:bg-primaryOne transition ease-in-out duration-500  flex items-center justify-center'>
-                        <BsFillPeopleFill className='text-primaryTwo group-hover:text-primaryOne transition ease-in-out duration-300 text-[1rem]' />
-                      </div>
-                      <p className="text-gray-600 font-semibold text-[1.1rem]">{followers} followers</p>
-                    </div>
-                  }
-                </div>
-              </div>
-    
-              <div className='flex justify-self-center my-20'>
-                <GoArrowSmallDown className='text-[2.5rem] text-orange-500 opacity-90'/>
-              </div>
-            </div>
-          </div>
-          <div>
-          </div>
-          <LBR languageBasedRepo={languageBasedRepo} />  
-          <PopularRepo popularRepos={popularRepos as unknown as [] || []}/>
-          <AllRepos allRepos={repos as unknown as [] || []}/>
-        </div>
-      }
+      
     </div>
   )
 }
 
 export async function getServerSideProps(context: any) {
-  const profileData = await getProfile(context.params.username)
+  // const profileData = await getProfile(context.params.username)
 
   return {
     props: {
